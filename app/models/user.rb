c0fdacb9b1e979ab10a9b42425f26bf6b17ab90f
Ashish <ashishkckse@gmail.com>
2020-05-06 18:04:19 +0530

    Add devise and rolify gems

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,23 @@
 class User < ApplicationRecord
+  rolify
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   enum status: [ :active, :inactive ]
+
+  after_create :assign_default_role
+
+  def admin?
+    has_role? :admin
+  end
+
+  def merchant?
+    has_role? :merchant
+  end
+
+  private
+    def assign_default_role
+      add_role(:merchant) if roles.blank?
+    end
 end
