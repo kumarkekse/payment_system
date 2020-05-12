27b0759a6d1bf4f3bb643179abeb2e756f88da5c
Ashish <ashishkckse@gmail.com>
2020-05-12 17:30:29 +0530

    Trigger transaction sum calculation after creating charge type transaction

--- app/models/charge_transaction.rb
+++ app/models/charge_transaction.rb
@@ -2,4 +2,12 @@ class ChargeTransaction < Transaction
   belongs_to :related_transaction, foreign_key: :transaction_id, class_name: 'Transaction'
 
   validates :amount, numericality: { greater_than: 0 }
+
+  after_create :after_process
+
+  private
+    def after_process
+      # adjust total_transaction_sum for merchant
+      related_transaction.user.save
+    end
 end
