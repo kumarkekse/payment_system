32a88792f4ba59d0713fb235d92feb4f9f3db125
Ashish <ashishkckse@gmail.com>
2020-05-12 17:02:34 +0530

    Show only merchant users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.with_role(:merchant)
   end
 
   # GET /users/1
